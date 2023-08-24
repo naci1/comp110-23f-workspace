8e17de9d78c7e59231fcfcef8c9fb8c1bd049e23
Narasimha Rao Cittarusu <naci@unc.edu>
2023-08-24 14:17:24 -0400

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,8 @@
+""" My first program for COMP110."""
+
+__author__ = "730652828"
+
+
+print("Hello, world. I am Narasimha Rao Cittarusu and I am excited to learn programming and look forward to COMP 110!")
+
+
