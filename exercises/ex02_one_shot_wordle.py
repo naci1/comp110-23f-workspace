f73be0bec16bc8f2f6efd7350e4cb7769a7f486b
Narasimha Rao Cittarusu <naci@unc.edu>
2023-09-14 02:10:25 -0400

    modified: made changes to one-shot-wordle work

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -14,13 +14,22 @@ while len(word_guess)!= len(secret_word) :
     next_guess : str = input("That was not 6 letters! Try again: ")
 index : int = 0
 emoji_string : str = ""
-while index < len(word_guess):
+while index < len(secret_word):
     if word_guess[index] == secret_word[index]:
         print(f"{GREEN_BOX}")
     else:
-        print(f"{WHITE_BOX}")
+        character_exists: bool = False
+        index_present : int = 0
+        while index_present == False and index_present < len(secret_word):
+            if word_guess[index_present] == secret_word[index]:
+                character_exists = True
+            else :
+                index_present +=1
+        if character_exists == True:
+            print(f"{YELLOW_BOX}")
+        else:
+            print(f"{WHITE_BOX}")
     index +=1
-
 if word_guess != secret_word:
     print("Not quite. Play again soon!")
 else :
