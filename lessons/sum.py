857249be03c8d1f1c69905f1c14f5abdcad94ec2
Narasimha Rao Cittarusu <naci@unc.edu>
2023-10-17 11:48:10 -0400

    modified : made changes to sum.py

--- lessons/sum.py
+++ lessons/sum.py
@@ -4,28 +4,28 @@ __author__ = "730652828"
 
 
 def w_sum(vals: list[float]) -> float:
-    """This calculates the sum of a list of floats using a while loop"""
-    counter: int = 0; # this allows us to iterate through the loop
-    sum: float = 0.0 # initializes the sum of the list to 0
+    """This calculates the sum of a list of floats using a while loop."""
+    counter: int = 0 
+    sum: float = 0.0 
     while counter < len(vals):
         sum += vals[counter]
         counter += 1
     return sum
 
 
-
 def f_sum(vals: list[float]) -> float:
-    """This calculates the sum of a list of floats using a for loop without range"""
-    sum: float = 0 #initializes the sum of the list to 0
+    """This calculates the sum of a list of floats using a for loop without range."""
+    sum: float = 0 
     for value in vals:
         sum += value
     return sum
+
 
 def f_range_sum(vals: list[float]) -> float:
     """This calculates the sum of a list of floats using a for loop with the "range" function"""
     if len(vals) == 0:
         return 0.0
-    sum: float = 0 #initializes the sum of the list to 0
+    sum: float = 0 
     for idx in range(len(vals)):
         sum += vals[idx]
     return sum
