336908ed0523dd504813f6df6a12ec8757929ffc
Narasimha Rao Cittarusu <naci@unc.edu>
2023-10-21 16:35:36 -0400

    modified: made changes to turtle_project

--- exercises/turtle_project.py
+++ exercises/turtle_project.py
@@ -16,20 +16,20 @@ def main() -> None:
     epic_tree_1: Turtle = Turtle()
     epic_tree_2: Turtle = Turtle()
     sun: Turtle = Turtle()
-    rectangle(ground , -300, -250, 600, 150, "green", "green")
-    house(epic_house ,-200 ,-100)
-    tree(epic_tree_1 , 100, -100, 1)
-    tree(epic_tree_2 , 200, -100, 0.5)
-    Circle(sun , -250, 165, 50, "yellow", "yellow", 360)
+    rectangle(ground, -300, -250, 600, 150, "green", "green")
+    house(epic_house, -200 ,-100)
+    tree(epic_tree_1, 100, -100, 1)
+    tree(epic_tree_2, 200, -100, 0.5)
+    Circle(sun, -250, 165, 50, "yellow", "yellow", 360)
     done()
 
 
-def rectangle(a_turtle: Turtle, x: float, y: float, length: float, width: float, fcolor:str, pcolor:str) -> None:
+def rectangle(a_turtle: Turtle, x: float, y: float, length: float, width: float, fcolor:str, pcolor: str) -> None:
     """This function creates a rectangle that can be used multiple times throughout the program."""
     a_turtle.speed(SPEED)
     a_turtle.hideturtle()
     a_turtle.penup()
-    a_turtle.goto(x,y)
+    a_turtle.goto(x, y)
     a_turtle.pendown()
     a_turtle.begin_fill()
     a_turtle.fillcolor(fcolor)
@@ -44,12 +44,12 @@ def rectangle(a_turtle: Turtle, x: float, y: float, length: float, width: float,
     a_turtle.end_fill()
 
 
-def triangle(a_turtle: Turtle, x: float, y: float, length: float, fcolor: str, pcolor:str) -> None:
+def triangle(a_turtle: Turtle, x: float, y: float, length: float, fcolor: str, pcolor: str) -> None:
     """This function provides the code for a triangle that can be used in our code."""
     a_turtle.speed(SPEED)
     a_turtle.hideturtle()
     a_turtle.penup()
-    a_turtle.goto(x,y)
+    a_turtle.goto(x, y)
     a_turtle.pendown()
     a_turtle.begin_fill()
     a_turtle.pencolor(pcolor)
@@ -58,7 +58,7 @@ def triangle(a_turtle: Turtle, x: float, y: float, length: float, fcolor: str, p
     while i < 3:
         a_turtle.forward(length)
         a_turtle.left(120) 
-        i = i+1
+        i += 1
     a_turtle.end_fill()
 
 
@@ -67,42 +67,42 @@ def house(a_turtle: Turtle, x: float, y: float) -> None:
     a_turtle.speed(SPEED)
     a_turtle.hideturtle()
     a_turtle.penup()
-    a_turtle.goto(x,y)
+    a_turtle.goto(x, y)
     a_turtle.pendown()
     rectangle(a_turtle, x, y, 190, 190, "yellow", "orange")
     triangle(a_turtle, x, y + 190, 190, "orange", "blue")
-    Door(a_turtle, x + (95/2), y, 95, 95)
+    Door(a_turtle, x + (95 / 2), y, 95, 95)
 
 
 def tree(a_turtle: Turtle, x: float, y: float, shrink_factor: float) -> None:
-    """
-    This function provides the code for a tree. The tree can not be taller than the house. Therefore, we introduced a shrink_factor to make the other trees smaller.
+    """This function provides the code for a tree. The tree can not be taller than the house. Therefore, we introduced a shrink_factor to make the other trees smaller.
+
     I also used the xcor() and ycor() command (to satisfy the "Above and beyond" category) to be used to find the position of x after creating a rectangle.
     """
     a_turtle.speed(SPEED)
     a_turtle.hideturtle()
     a_turtle.penup()
-    a_turtle.goto(x,y)
+    a_turtle.goto(x, y)
     a_turtle.pendown()
-    rectangle(a_turtle, x, y, 50*shrink_factor, 200*shrink_factor, "brown", "brown")
-    rectangle(a_turtle, x-(50*shrink_factor), y+(200*shrink_factor), 150*shrink_factor , 50*shrink_factor, "green", "green")
+    rectangle(a_turtle, x, y, 50 * shrink_factor, 200 * shrink_factor, "brown", "brown")
+    rectangle(a_turtle, x - (50 * shrink_factor), y + (200 * shrink_factor), 150 * shrink_factor , 50 * shrink_factor, "green", "green")
     new_pos_x: float = a_turtle.xcor()
     new_pos_y: float = a_turtle.ycor()
-    a_turtle.forward(150*shrink_factor)
+    a_turtle.forward(150 * shrink_factor)
     a_turtle.left(90)
-    a_turtle.forward(50*shrink_factor)
-    Circle(a_turtle, new_pos_x + 150*shrink_factor, new_pos_y + 50*shrink_factor, (150*shrink_factor)/2, "green", "green", 180)
+    a_turtle.forward(50 * shrink_factor)
+    Circle(a_turtle, new_pos_x + 150 * shrink_factor, new_pos_y + 50 * shrink_factor, (150 * shrink_factor) / 2, "green", "green", 180)
 
 
-def Circle(a_turtle: Turtle, x: float, y: float, radius: float, fcolor: str, pcolor: str, angle:float) -> None:
-    """
-    This method makes use of the built in circle command (to satisfy the "Above and beyond" category) to be used multiple times throughout the program.
+def Circle(a_turtle: Turtle, x: float, y: float, radius: float, fcolor: str, pcolor: str, angle: float) -> None:
+    """This method makes use of the built in circle command (to satisfy the "Above and beyond" category) to be used multiple times throughout the program.
+
     It uses the fill color and pencolor to change the border and fill color of the circles.
     """
     a_turtle.speed(SPEED)
     a_turtle.hideturtle()
     a_turtle.penup()
-    a_turtle.goto(x,y)
+    a_turtle.goto(x, y)
     a_turtle.pendown()
     a_turtle.begin_fill()
     a_turtle.fillcolor(fcolor)
