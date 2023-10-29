2760459936e63aa5e81a6754a7b178add575c308
Narasimha Rao Cittarusu <naci@unc.edu>
2023-10-29 16:17:47 -0400

    modified: made changes to dictionary.py

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -2,33 +2,72 @@
 
 __author__ = "730652828"
 
-def invert(given_dict: dict[str, str]) -> dict[str,str]:
-    keys_of_dict: list[str] = []
-    values_of_dict: list[str] = []
-    for key in given_dict:
-        keys_of_dict.append(key)
-        values_of_dict.append(given_dict[key])
-    
-    # define an inverted list
-    inverted_dict: dict[str, str] = {}
 
-    # iterate through one of the lists
-    for i in range(len(given_dict)):
-        inverted_dict[values_of_dict[i]] = keys_of_dict[i]    
-    # check whether the keys in the inverted_dict are duplicated or not
-    for value in given_dict:
-        if value in given_dict == value in inverted_dict:
-            raise KeyError("duplicate keys are present in the inverted dictionary.")
-  
+def invert(given_dict: dict[str, str]) -> dict[str, str]:
+    """This function takes a given_dict and inverts the values of the list by switching the key and value pairs and returns the new inverted dict. If two values in the original list are the same a Key_error will be produced."""
+    inverted_dict: dict[str,str] = {}
+    temp_value = ""
+    for key in given_dict:
+        temp_value = given_dict[key]
+        if temp_value in inverted_dict:
+            raise KeyError("Multiple keys are present in the inverted list!")
+        else:
+            inverted_dict[temp_value] = key
     return inverted_dict
 
 
-def favorite_colors(given_dict: dict[str,str]) -> str:
-    counter: int = 0
-    while counter < len(given_dict):
-        
-        
-    
+def favorite_color(given_dict: dict[str, str]) -> str:
+    """This function takes a given_dict and returns a string for the value that appears most in the list. If two values appear the most the same amount of times, the function returns the value that appeared first in the list."""
+    temp_color: str = ""
+    temp_counter: int = 0
+    max_color_counter: int = 0
+    max_color_appearance: str = ""
+    for key in given_dict:
+        temp_color = given_dict[key]
+        temp_counter = 0
+        for key in given_dict:
+            if given_dict[key] == temp_color:
+                temp_counter += 1
+        if temp_counter > max_color_counter:
+            max_color_counter = temp_counter
+            max_color_appearance = temp_color
+    return max_color_appearance
 
-    
-    
+
+def count(given_list: list[str]) -> dict[str, int]:
+    """This function produces a dictionary which contains a key and the value of the key is the number of times that key appears in the list."""
+    empty_dict: dict[str, int] = {}
+    for key in given_list:
+        if key in given_list:
+            empty_dict[key] += 1
+        else:
+            empty_dict[key] = 1
+    return empty_dict
+
+
+def alphabetizer(given_list: list[str]) -> dict[str, list[str]]:
+    """This function returns a dictionary where the keys are the first letters of the word and values are a list that contain all the words that begin with that specific key letter."""
+    alphabetized_list: dict[str, list[str]] = {}
+    used_letters: list[str] = []
+    temp_first_letter: str = 0
+    for word in given_list:
+        temp_first_letter = word[0].lower()
+        if temp_first_letter not in used_letters:
+            letter_list: list[str] = []
+            for word in given_list:
+                if word[0].lower() == temp_first_letter:
+                    letter_list.append(word)
+                    alphabetized_list[temp_first_letter] = letter_list
+        used_letters.append(temp_first_letter)
+    return alphabetized_list
+
+
+def update_attendance(given_dict: dict[str, list[str]], day: str, student: str) -> dict[str, list[str]]:
+    """This function returns a dictionary updated with a day as the key and a value as the updated value."""
+    if day in given_dict:
+        given_dict[day] += student
+    else:
+        given_dict[day] = student
+    return given_dict
+        
+
