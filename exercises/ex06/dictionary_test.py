f8ba6635a4d5be3e4b4ccb3b11e6828a54919bf6
Narasimha Rao Cittarusu <naci@unc.edu>
2023-11-05 16:33:09 -0500

    modified: made changes to dictionary_test.py

--- exercises/ex06/dictionary_test.py
+++ exercises/ex06/dictionary_test.py
@@ -2,7 +2,7 @@
 
 __author__ = "730652828"
 
-from dictionary import invert, favorite_color, count, alphabetizer, update_attendance
+from exercises.ex06.dictionary import invert, favorite_color, count, alphabetizer, update_attendance
 
 import pytest
 
